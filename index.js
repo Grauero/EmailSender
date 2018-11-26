0ec74295f59dccdd20ac9418ed8f207502eb9899
Grauero <grayero6996@gmail.com>
2018-11-26 22:35:50 +0300

    Added passport, and authentication with google

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 const express = require('express');
+const passport = require('passport');
+const GoogleStrategy = require('passport-google-oauth20').Strategy; 
 
 const app = express();
 const PORT = process.env.PORT || 5000;
@@ -6,3 +8,7 @@ const PORT = process.env.PORT || 5000;
 app.get('/', (req, res) => {
   res.send();
 });
+
+passport.use(new GoogleStrategy()); 
+
+app.listen(PORT);
