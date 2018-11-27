333966a6318844fcddbf5ea931649bbaebea4340
Grauero <grayero6996@gmail.com>
2018-11-27 18:00:21 +0300

    Implemented auth with google acc

--- index.js
+++ index.js
@@ -1,14 +1,30 @@
 const express = require('express');
 const passport = require('passport');
-const GoogleStrategy = require('passport-google-oauth20').Strategy; 
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
+
+const keys = require('./config/keys');
 
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-app.get('/', (req, res) => {
-  res.send();
-});
+passport.use(
+  new GoogleStrategy(
+    {
+      clientID: keys.googleClientID,
+      clientSecret: keys.googleClientSecret,
+      callbackURL: '/auth/google/callback'
+    },
+    (accessToken, refreshToken, profile, done) => console.log(accessToken)
+  )
+);
 
-passport.use(new GoogleStrategy()); 
+app.get(
+  '/auth/google',
+  passport.authenticate('google', {
+    scope: ['profile', 'email']
+  })
+);
+
+app.get('/auth/google/callback', passport.authenticate('google'));
 
 app.listen(PORT);
