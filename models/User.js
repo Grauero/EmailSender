e2a4ed4b303c723655f464452d1e6458548c29fe
Grauero <grayero6996@gmail.com>
2018-11-28 16:20:45 +0300

    Created model for users collections in mongoDB && connected auth proccess to mongoDB

---
+++ models/User.js
@@ -0,0 +1,12 @@
+const mongoose = require('mongoose');
+
+const userSchema = new mongoose.Schema({
+  googleId: {
+    type: String,
+    required: true
+  }
+});
+
+const User = mongoose.model('users', userSchema);
+
+module.exports = User;
--- services/passport.js
+++ services/passport.js
@@ -1,6 +1,7 @@
 const passport = require('passport');
 const GoogleStrategy = require('passport-google-oauth20').Strategy;
 
+const User = require('../models/User');
 const keys = require('../config/keys');
 
 passport.use(
@@ -10,6 +11,16 @@ passport.use(
       clientSecret: keys.googleClientSecret,
       callbackURL: '/auth/google/callback'
     },
-    (accessToken, refreshToken, profile, done) => console.log(accessToken)
+    (accessToken, refreshToken, profile, done) => {
+      User.findOne({ googleId: profile.id }).then(existingUser => {
+        if (existingUser) {
+          done(null, existingUser);
+        } else {
+          new User({ googleId: profile.id })
+            .save()
+            .then(newUser => done(null, newUser));
+        }
+      });
+    }
   )
 );
