897d8eebd28193fe935c7e1256391259e2951c2d
Grauero <grayero6996@gmail.com>
2018-12-01 23:24:25 +0300

    Added authReducer, configured routes boilerplate

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,11 +1,26 @@
 import React from 'react';
 import { Provider } from 'react-redux';
+import { BrowserRouter, Route } from 'react-router-dom';
 
 import store from '../store/store';
 
+const Header = () => <h2>Header</h2>;
+const Dashboard = () => <h2>Dashboard</h2>;
+const SurveyNew = () => <h2>SurveyNew</h2>;
+const Landing = () => <h2>Landing</h2>;
+
 const App = () => (
   <Provider store={store}>
-    <div>123</div>
+    <div>
+      <BrowserRouter>
+        <div>
+          <Header />
+          <Route exact path="/" component={Landing} />
+          <Route exact path="/surveys" component={Dashboard} />
+          <Route exact path="/surveys/new" component={SurveyNew} />
+        </div>
+      </BrowserRouter>
+    </div>
   </Provider>
 );
 
