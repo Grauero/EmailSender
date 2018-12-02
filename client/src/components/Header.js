0372ba9f079ea59c1780ead4ce5b87bc697af274
Grauero <grayero6996@gmail.com>
2018-12-02 22:23:01 +0300

    Connected Header component to auth flow

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,18 +1,51 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
 
-const Header = () => (
-  <nav>
-    <div className="naw-wrapper">
-      <a href=" " className="left brand-logo">
-        Email Sender
-      </a>
-      <ul className="right">
-        <li>
-          <a href=" ">Login With Google</a>
-        </li>
-      </ul>
-    </div>
-  </nav>
-);
+import { fetchUser } from '../store/actions';
 
-export default Header;
+const Header = ({ auth }) => {
+  function renderContent() {
+    switch (auth) {
+      case null:
+        return null;
+      case false:
+        return (
+          <li>
+            <a href="/auth/google">Login With Google</a>
+          </li>
+        );
+      default:
+        return (
+          <li>
+            <a href=" ">Logout</a>
+          </li>
+        );
+    }
+  }
+
+  return (
+    <nav>
+      <div className="naw-wrapper">
+        <a href=" " className="left brand-logo">
+          Email Sender
+        </a>
+        <ul className="right">{renderContent()}</ul>
+      </div>
+    </nav>
+  );
+};
+
+Header.propTypes = {
+  auth: PropTypes.instanceOf(Object).isRequired,
+  fetchUser: PropTypes.func.isRequired
+};
+
+const mapStateToProps = state => ({
+  auth: state.auth
+});
+
+export default connect(
+  mapStateToProps,
+  { fetchUser }
+)(Header);
