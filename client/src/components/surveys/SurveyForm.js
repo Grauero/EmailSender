40210b298a8b98fc2662e2ada88c9f578fc1eb1a
Grauero <grayero6996@gmail.com>
2018-12-09 18:46:26 +0300

    Created SurveyForm, SurveyField components

--- client/src/components/surveys/SurveyForm.js
+++ client/src/components/surveys/SurveyForm.js
@@ -1,19 +1,45 @@
-import React, { Component } from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
+import { Link } from 'react-router-dom';
 import { reduxForm, Field } from 'redux-form';
 
-class SurveyForm extends Component {
-  render() {
-    return (
-      <div>
-        <form onSubmit={this.props.handleSubmit(values => console.log(values))}>
-          <Field type="text" name="surveyTitle" component="input" />
-          <button type="submit">Submit</button>
-        </form>
-      </div>
-    );
+import SurveyField from './SurveyField';
+
+const SurveyForm = ({ handleSubmit }) => {
+  const FIELDS = [
+    { label: 'Survey Title', name: 'title' },
+    { label: 'Subject Line', name: 'subject' },
+    { label: 'Email Body', name: 'body' },
+    { label: 'Recipient List', name: 'emails' }
+  ];
+
+  function renderFields() {
+    return FIELDS.map(({ label, name }) => (
+      <Field
+        key={name}
+        component={SurveyField}
+        type="text"
+        label={label}
+        name={name}
+      />
+    ));
   }
-}
+
+  return (
+    <div>
+      <form onSubmit={handleSubmit(values => console.log(values))}>
+        {renderFields()}
+        <Link to="/surveys" className="red btn-flat white-text">
+          Cancel
+        </Link>
+        <button type="submit" className="teal btn-flat right white-text">
+          Next
+          <i className="material-icons right">done</i>
+        </button>
+      </form>
+    </div>
+  );
+};
 
 SurveyForm.propTypes = {
   handleSubmit: PropTypes.func.isRequired
